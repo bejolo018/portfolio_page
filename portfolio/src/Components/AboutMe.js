57e07ac97ba0f49aa442db78db6ed9dbf7605024
bejolo018 <bejolo018@gmail.com>
2021-02-09 23:05:28 -0600

    Tweaked about me page, revised about me section, added name tags to icons, replaced image of self with svg

--- portfolio/src/Components/AboutMe.js
+++ portfolio/src/Components/AboutMe.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import me from '../Assets/me.jpg'
+import me from '../Assets/me.png'
 import { AiOutlineLinkedin } from 'react-icons/ai'
 import { RiGithubLine, RiPagesLine } from 'react-icons/ri'
 import Pdf from '../Assets/Resume3.pdf'
@@ -7,12 +7,12 @@ import Pdf from '../Assets/Resume3.pdf'
 function AboutMe() {
     return (
         <div>
-            <img src={me} alt='me' className="block m-auto sm:max-w-full sm:p-0 mb-4 mt-12" style={{maxWidth: "300px", borderRadius: '20px'}}/>
+            <img src={me} alt='me' className="block m-auto sm:max-w-xsm sm:p-0 mb-4 mt-12" style={{borderRadius: '20px'}}/>
 
             <div style={{color: 'white'}}>
-                <a className='inline-block mb-5' href="https://www.linkedin.com/in/benjaminjohnlopez/"><AiOutlineLinkedin size={50}/></a>     
-                <a className='inline-block mb-5' href="https://github.com/bejolo018"><RiGithubLine size={50}/></a> 
-                <a className='inline-block mb-5' href={Pdf} target="_blank" rel="noopener noreferrer"><RiPagesLine size={50}/></a> 
+                <a className='inline-block m-5' href="https://www.linkedin.com/in/benjaminjohnlopez/"><AiOutlineLinkedin size={50}/> LinkedIn</a>     
+                <a className='inline-block m-5' href="https://github.com/bejolo018"><RiGithubLine size={50}/>Github</a> 
+                <a className='inline-block m-5' href={Pdf} target="_blank" rel="noopener noreferrer"><RiPagesLine size={50}/>Resume</a> 
             </div>
             
             {/* <a href="https://www.figma.com/files/project/11402597/Benjamin's-Design-Portfolio"
@@ -21,7 +21,10 @@ function AboutMe() {
             > Design Portfolio</a> */}
 
             <div className="mb-10 text-xl" style={{color: 'white'}}>
-            Full Stack Web Developer with a passion for design.   
+            Driven software engineer
+            who loves working on
+            and managing awesome
+            projects. 
             </div> 
 
         </div>
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -2,13 +2,15 @@ module.exports = {
   purge: [],
   theme: {
     screens: {
-      'xsm': '420px',
       'lg' : '1280px'
     },
     extend: {
       backgroundColor:{
         'black-t-50': 'rgba(0,0,0,0.5)'
       },
+      screens: {
+        'xsm': '320px'
+      }
     },
   },
   variants: {},
