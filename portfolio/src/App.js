4be837dea2f6e7872aff78d06382f6e94289cfe9
bejolo018 <bejolo018@gmail.com>
2021-02-11 14:32:13 -0600

    Fixed spelling error, reorganized file structure to trim the fat and get ready to implement react router

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import Projects from './Components/Projects'
-import Home from './Views/Home'
 import ContactMe from './Components/ContactMe'
 import projectData from './projectData'
 import './index.css'
+import AboutMe from './Components/AboutMe'
+import Technologies from './Components/Technologies'
 
 class App extends Component {
   state = {
@@ -13,19 +14,19 @@ class App extends Component {
 render(){
   return (
     <div className="pb-10 min-h-screen">      
-      <div>
-          <Home />
-          <div className="fadein3">
-
-            <div className='lg:px-56'>
-              <Projects projects={this.state.projects}/>
-            </div>
-
-            <div className='mt-10' style={{textAlign: "center"}} >
-              <ContactMe />
-            </div>
-          
+        <div className="fadein" style={{textAlign: "center"}}>
+              <AboutMe />
+          </div>
+        <div className="fadein2" >
+              <Technologies />
         </div>
+        <div className="fadein3">
+          <div className='lg:px-56'>
+            <Projects projects={this.state.projects}/>
+          </div>
+          <div className='mt-10' style={{textAlign: "center"}} >
+            <ContactMe />
+          </div>
       </div>
   </div>
   )
--- portfolio/src/Components/AboutMe.js
+++ portfolio/src/Components/AboutMe.js
@@ -7,12 +7,11 @@ import Pdf from '../Assets/Resume3.pdf'
 function AboutMe() {
     return (
         <div>
+            <div className="font-bold mb-3" style={{fontSize: "70px", color: 'white'}}>Benjamin Lopez</div>
             <img src={me} alt='me' className="m-auto sm:max-w-xsm mb-4 mt-12" style={{borderRadius: '20px'}}/>
-
             <div className="mt-12 mb-10 text-xl" style={{color: 'white'}}>
             Driven software engineer who loves working on and managing awesome projects. 
             </div> 
-
             <div style={{color: 'white'}}>
                 <a className='inline-block m-5' href="https://www.linkedin.com/in/benjaminjohnlopez/"><AiOutlineLinkedin size={50}/> LinkedIn</a>     
                 <a className='inline-block m-5' href="https://github.com/bejolo018"><RiGithubLine size={50}/>Github</a> 
