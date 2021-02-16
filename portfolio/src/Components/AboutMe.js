59aba62b0df26091e41b590e18196b049aa092cd
bejolo018 <bejolo018@gmail.com>
2021-02-16 17:48:46 -0600

    updated resume and about me section

--- portfolio/src/Components/AboutMe.js
+++ portfolio/src/Components/AboutMe.js
@@ -10,7 +10,7 @@ function AboutMe() {
             <div className="font-bold mb-3" style={{fontSize: "70px", color: 'white'}}>Benjamin Lopez</div>
             <img src={me} alt='me' className="m-auto sm:max-w-xsm mb-4 mt-12" style={{borderRadius: '20px'}}/>
             <div className="mt-12 mb-10 text-xl" style={{color: 'white'}}>
-            Driven software engineer who loves working on and managing awesome projects. 
+            Driven software engineer who loves working with awesome teams on awesome projects.
             </div> 
             <div style={{color: 'white'}}>
                 <a className='inline-block m-5' href="https://www.linkedin.com/in/benjaminjohnlopez/"><AiOutlineLinkedin size={50}/> LinkedIn</a>     
